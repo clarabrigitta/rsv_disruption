1d5bb3a90715478165d3bb0b2324b296ebda5ff7
clarabrigitta <clara.brigitta@gmail.com>
2024-03-29 14:28:57 +0100

    add option to choose disruption year in model plus curation, change order of curation to be more intuitive

--- code/model/model_births.R
+++ code/model/model_births.R
@@ -19,7 +19,7 @@ data <- read_excel("./data/births_pregnancies.xlsx", sheet = "All") %>%
 # -------------------------------------------------------------------------
 
 # infection and disease model for monthly births
-model_births <- function(month_chosen = c()){
+model_births <- function(month_chosen = c(), disruption_year = c()){
   rep <- 3 # number of years
   rate_scale <- 5 # scaling rate of infection to achieve 90-95% in first 3yrs of life
   
@@ -67,7 +67,7 @@ model_births <- function(month_chosen = c()){
   
   # join women's immunity level proportions to output
   output <- output %>% 
-    left_join(women.prop %>% select(-count)) %>% 
+    left_join(women.prop %>% select(-count) %>% filter(disruption == disruption_year)) %>% 
     # set probability of infection/disease based on immunity level
     mutate(prob_inf = case_when(level == 1 ~ 0,
                                 level == 2 ~ 0.5,
@@ -136,15 +136,60 @@ model_births <- function(month_chosen = c()){
 
 # model one year of monthly births
 for(m in month.abb){
-  model_births(month_chosen = m)
+  model_births(month_chosen = m, disruption_year = 0)
 }
+
+# -------------------------------------------------------------------------
+
+# determine age at infection and disease
+
+for(m in 1:12){
+  assign(paste0("births_", month.abb[m]), readRDS(file = paste0("./output/data/monthly/births_", month.abb[m], ".rds")) %>% mutate(month_born = month.abb[m]))
+}
+
+# month of age
+births_age_month <- bind_rows(births_Jan, births_Feb, births_Mar, births_Apr, births_May, births_Jun, births_Jul, births_Aug, births_Sep, births_Oct, births_Nov, births_Dec) %>% 
+  mutate(month_born = factor(month_born, levels = month.abb),
+         population = case_when(month_born == "Jan" ~ 61942,
+                                month_born == "Feb" ~ 59783,
+                                month_born == "Mar" ~ 61043,
+                                month_born == "Apr" ~ 58088,
+                                month_born == "May" ~ 62736,
+                                month_born == "Jun" ~ 59664,
+                                month_born == "Jul" ~ 61920,
+                                month_born == "Aug" ~ 61995,
+                                month_born == "Sep" ~ 62949,
+                                month_born == "Oct" ~ 63387,
+                                month_born == "Nov" ~ 59593,
+                                month_born == "Dec" ~ 59574))
+
+saveRDS(births_age_month, file = "./output/data/births_age_month.rds")
+
+# age groups
+births_age <- bind_rows(births_Jan, births_Feb, births_Mar, births_Apr, births_May, births_Jun, births_Jul, births_Aug, births_Sep, births_Oct, births_Nov, births_Dec) %>% 
+  mutate(month_born = factor(month_born, levels = month.abb),
+         `<6` = ifelse(time <= 6, 1, 0),
+         `6-12` = ifelse(time >= 6 & time <= 12, 1, 0),
+         `12-24` = ifelse(time >= 12 & time <= 24, 1, 0),
+         `24-36` = ifelse(time >= 24 & time <= 36, 1, 0)) %>%
+  select(-time) %>% 
+  pivot_longer(cols = c("<6", "6-12", "12-24", "24-36"), names_to = "age", values_to = "value") %>%
+  mutate(age = factor(age, levels = c("<6", "6-12", "12-24", "24-36"))) %>% 
+  filter(value == 1) %>% 
+  select(-value) %>% 
+  group_by(level, month_born, age) %>% 
+  summarise(infected = sum(infected),
+            disease = sum(disease)) %>% 
+  pivot_longer(cols = c("infected", "disease"), names_to = "type", values_to = "count")
+
+saveRDS(births_age, file = "./output/data/births_age.rds")
 
 # -------------------------------------------------------------------------
 
 # combine monthly modeled data into 1 yearly dataset
 
 for(m in 1:12){
-  data <- get(paste0("births_", month.abb[m])) %>% 
+  data <- readRDS(file = paste0("./output/data/monthly/births_", month.abb[m], ".rds")) %>% 
     mutate(month_born = month.abb[m],
            time = m:(35+m)) %>% 
     ungroup()
@@ -169,70 +214,56 @@ births_year <- bind_rows(births_Jan, births_Feb, births_Mar, births_Apr, births_
 
 saveRDS(births_year, file = "./output/data/births_year.rds")
 
+# yearly dataset with distribution of infection/disease per season by month of birth
 
-# -------------------------------------------------------------------------
-
-# determine age at infection and disease
-
-for(m in 1:12){
-  assign(paste0("births_", month.abb[m]), readRDS(file = paste0("/Users/lsh2301561/Desktop/rsv_disruption/output/data/monthly/births_", month.abb[m],".rds")))
-    
-  data <- get(paste0("births_", month.abb[m])) %>% 
-      mutate(month_born = month.abb[m])
-    
-    assign(paste0("births_", month.abb[m]), data)
-}
-
-births_age <- bind_rows(births_Jan, births_Feb, births_Mar, births_Apr, births_May, births_Jun, births_Jul, births_Aug, births_Sep, births_Oct, births_Nov, births_Dec) %>% 
-  mutate(month_born = factor(month_born, levels = month.abb),
-         `<6` = ifelse(time <= 6, 1, 0),
-         `6-12` = ifelse(time >= 6 & time <= 12, 1, 0),
-         `12-24` = ifelse(time >= 12 & time <= 24, 1, 0),
-         `24-36` = ifelse(time >= 24 & time <= 36, 1, 0)) %>%
-  select(-time) %>% 
-  pivot_longer(cols = c("<6", "6-12", "12-24", "24-36"), names_to = "age", values_to = "value") %>%
-  mutate(age = factor(age, levels = c("<6", "6-12", "12-24", "24-36"))) %>% 
-  filter(value == 1) %>% 
-  select(-value) %>% 
-  group_by(level, month_born, age) %>% 
+births_season <- births_year %>% 
+  filter(level == "total") %>% 
+  filter(time >=7 & time <=42) %>% 
+  mutate(season = case_when(time >= 7 & time <= 18 ~ 1,
+                            time >= 19 & time <= 30 ~ 2,
+                            time >= 31 & time <= 42 ~ 3)) %>% 
+  group_by(season, month_born) %>% 
   summarise(infected = sum(infected),
-            disease = sum(disease)) %>% 
-  pivot_longer(cols = c("infected", "disease"), names_to = "type", values_to = "count")
+            disease = sum(disease))
 
-saveRDS(births_age, file = "./output/data/births_age.rds")
+saveRDS(births_season, file = "./output/data/births_season.rds")
 
+births_season %>% 
+  plot_ly() %>%
+  add_trace(x = ~season,
+            y = ~infected,
+            split = ~month_born,
+            color = ~month_born,
+            type = "bar",
+            legendgroup = ~month_born,
+            showlegend = TRUE) %>% 
+  layout(yaxis = list(title = "Infection Count",
+                      range = list(0, 30000),
+                      tickmode = "linear",
+                      tick0 = 0,
+                      dtick = 5000,
+                      tickformat = "digits"),
+         xaxis = list(title = "Season (Jul-Jun)"))
 
 # -------------------------------------------------------------------------
 
-plot_cumprop <- function(data){
-  fig <- data %>%
-    filter(level == "total") %>%
-    group_by(month_born) %>%
-    mutate(cum_sum = cumsum(disease),
-           prop = (cum_sum/population)*100) %>%
-    plot_ly() %>%
-    add_trace(x = ~time,
-              y = ~prop,
-              split = ~month_born,
-              color = ~month_born,
-              type = "scatter",
-              mode = "lines",
-              hovertemplate = paste('<b>Age (months)</b>: %{x}',
-                                    '<br><b>Proportion Infected</b>: %{y}',
-                                    '<extra></extra>'),
-              showlegend = TRUE) %>%
-    layout(xaxis = list(title = "Month of age",
-                        range = list(0, 36),
-                        tickmode = "linear",
-                        tick0 = 0,
-                        dtick = 3),
-           yaxis = list(title = "Proportion Infected (%)",
-                        range = list(0, 100),
-                        tickmode = "linear",
-                        tick0 = 0,
-                        dtick = 20))
-  
-  return(fig)
-}
+# need to repeat lines 137-215 for each disruption category by specifying disruption_year argument in model
 
-plot_cumprop(births_age_month)
+# combining different different immunity proportions
+births_year_disrupt <- bind_rows(readRDS(file = "./output/data/disruption/births_year_0.rds") %>% mutate(disruption = 0),
+                                 readRDS(file = "./output/data/disruption/births_year_1.rds") %>% mutate(disruption = 1),
+                                 readRDS(file = "./output/data/disruption/births_year_2.rds") %>% mutate(disruption = 2),
+                                 readRDS(file = "./output/data/disruption/births_year_3.rds") %>% mutate(disruption = 3))
+saveRDS(births_year_disrupt, file = "./output/data/births_year_disrupt.rds")
+
+births_age_disrupt <- bind_rows(readRDS(file = "./output/data/disruption/births_age_0.rds") %>% mutate(disruption = 0),
+                                readRDS(file = "./output/data/disruption/births_age_1.rds") %>% mutate(disruption = 1),
+                                readRDS(file = "./output/data/disruption/births_age_2.rds") %>% mutate(disruption = 2),
+                                readRDS(file = "./output/data/disruption/births_age_3.rds") %>% mutate(disruption = 3))
+saveRDS(births_age_disrupt, file = "./output/data/births_age_disrupt.rds")
+
+births_age_month_disrupt <- bind_rows(readRDS(file = "./output/data/disruption/births_age_month_0.rds") %>% mutate(disruption = 0),
+                                      readRDS(file = "./output/data/disruption/births_age_month_1.rds") %>% mutate(disruption = 1),
+                                      readRDS(file = "./output/data/disruption/births_age_month_2.rds") %>% mutate(disruption = 2),
+                                      readRDS(file = "./output/data/disruption/births_age_month_3.rds") %>% mutate(disruption = 3))
+saveRDS(births_age_month_disrupt, file = "./output/data/births_age_month_disrupt.rds")
