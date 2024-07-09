82de6317feb716f3b0fd7df1b64b1a06d5beba39
clarabrigitta <clara.brigitta@gmail.com>
2024-07-09 19:14:14 +0100

    optimise model function

--- code/model/model_function.R
+++ code/model/model_function.R
@@ -8,25 +8,10 @@ library(purrr)
 # -------------------------------------------------------------------------
 # creating fixed data to use in model function
 
-# starting matrix for modelling women
+#fixed parameters
 n_interest <- 24 # number of months of interest for history of infection/ duration of immunity
 rep <- 30 # number of years to model
 n_burn <- 20 # burn-in period before disruption
-
-women_mat <- as.data.frame(matrix(0, 12*rep, 5+n_interest))
-colnames(women_mat) <- c("time", "month", "rate", "susceptible_naive", "susceptible_reinf", str_c(rep("I", n_interest), 1:n_interest))
-
-women_mat <- women_mat %>% mutate(month = rep(1:12, rep),
-                              time = 1:nrow(women_mat),
-                              rate = (case_when(month == 1 ~ 0.06,
-                                                month == 2 | month == 3 ~ 0.02,
-                                                month >= 4 & month <= 8 ~ 0.000,
-                                                month == 9 ~ 0.04,
-                                                month == 10 ~ 0.08,
-                                                month == 11 | month == 12 ~ 0.18))) %>% 
-  select(-month)
-
-women_mat <- apply(as.matrix(women_mat), c(1, 2), as.numeric)
 
 # helper data frame for dates and rates (without any disruption) to model babies
 dates <- as.data.frame(matrix(NA, 12*(rep+4), 6))
@@ -50,7 +35,7 @@ dates <- dates %>%
   left_join(data.frame(level = rep(1:25, 264),
                        time = rep(1:264, each = 25)))
 
-# matrix for Scottish birth data (2012 - 2029)
+# helper data frame for Scottish birth data (2012 - 2029)
 birth_data <- read_excel("./data/births-time-series-22-bt.3.xlsx", skip = 3) %>%
   head(-7) %>%
   select(1:14) %>%
@@ -64,9 +49,27 @@ birth_data <- read_excel("./data/births-time-series-22-bt.3.xlsx", skip = 3) %>%
   right_join(dates %>% select(-c(rate, level)) %>% filter(year <= 2029) %>% distinct()) %>%
   # extrapolate births beyond 2022 (linear regression)
   mutate(births = ifelse(is.na(births), -8.131*time + 4903.856, births)) %>% 
-  select(c(time, births))
+  select(births) %>% 
+  pull()
 
-birth_data <- as.matrix(birth_data)
+# starting matrix for modelling women
+women_mat <- as.data.frame(matrix(0, 12*rep, 5+n_interest))
+colnames(women_mat) <- c("time", "month", "rate", "susceptible_naive", "susceptible_reinf", str_c(rep("I", n_interest), 1:n_interest))
+
+women_mat <- women_mat %>% mutate(month = rep(1:12, rep),
+                              time = 1:nrow(women_mat),
+                              rate = case_when(month == 1 ~ 0.06,
+                                                month == 2 | month == 3 ~ 0.02,
+                                                month >= 4 & month <= 8 ~ 0.000,
+                                                month == 9 ~ 0.04,
+                                                month == 10 ~ 0.08,
+                                                month == 11 | month == 12 ~ 0.18),
+                              births = 0) %>% 
+  select(-month)
+
+women_mat[145:360, "births"] <- birth_data # combining monthly birth data with women matrix
+
+women_mat <- apply(as.matrix(women_mat), c(1, 2), as.numeric)
 
 # vector for population size of <1 and 1-4 in Scotland
 population <- read_excel("./data/mid-year-pop-est-22-data.xlsx", sheet = "Table 1", skip = 3) %>%
@@ -82,16 +85,19 @@ population <- read_excel("./data/mid-year-pop-est-22-data.xlsx", sheet = "Table 
   pull()
 
 # empty matrix to model babies for 4 years
-empty <- as.data.frame(matrix(0, 48, 10))
-colnames(empty) <- c("time_calendar", "time_birth", "level", "rate", "prob_inf", "waning", "aging", "susceptible", "infected", "disease")
-
+empty <- as.data.frame(matrix(0, 48, 28))
+colnames(empty) <- c("time_calendar", "rate", "susceptible_reinf", str_c(rep("I", n_interest), 1:n_interest), "births") # , "time_birth", "prob_inf", "waning", "aging", "susceptible", "infected", "disease")
+empty <- cbind(empty, time_birth = 1:48)
 empty <- as.matrix(empty)
 
 # vector of monthly rates
 rate_vector <- dates %>% select(-level) %>% distinct() %>% select(rate) %>% pull()
 
+# vector of levels
+level <- c(25, 1:24)
+
 # put all data into a list
-save_data <- list(women_mat, birth_data, population, empty, rate_vector)
+save_data <- list(women_mat, empty, rate_vector, population, level)
 
 # -------------------------------------------------------------------------
 
@@ -105,6 +111,7 @@ model_function <- function(lambda, theta, omega, alpha, stored_data){
   # 1 = time
   # 2 = rate
   # 3 - 28 = infection history status
+  # 29 = births
   women <- stored_data[[1]]
   
   # subject rate to disruption factor lambda 
@@ -113,9 +120,9 @@ model_function <- function(lambda, theta, omega, alpha, stored_data){
   # initial state
   women[1, 3] <- 1000000
   
-  # model women
+  ## model women
   for (row in 3:nrow(women)) {
-    # map_dbl(2:nrow(women),
+    # map_dbl(3:nrow(women),
     #     function(x) {})
     
     women[row - 1, 5] <- women[row - 2, 3] * women[row - 2, 2] + women[row - 2, 4] * women[row - 2, 2]
@@ -123,89 +130,78 @@ model_function <- function(lambda, theta, omega, alpha, stored_data){
     women[row - 1, 4] <- women[row - 2, 4] - women[row - 2, 4] * women[row - 2, 2] + women[row - 2, 28]
     women[row, c(6:28, 4)] <- women[row - 1, 5:28]
     
-    # <- c(women[row - 1. month_at:last_month], c(1:))
-    # for (month in rev(2:24)){
-    #     women[row, 30-month] <- women[row - 1, 29-month]
-    # }
   }
   
   # create link between mothers and babies (infection history/immunity split)
-  women <- cbind(women, sum = 1000000)
   women <- women[145:360, ] # selecting for 2012-2029 to match time period of birth data
   women[, 1] <- 1:216 # re-labeling time to match birth data
   
-  immunity_split <- as.data.frame(women) %>% 
-    pivot_longer(4:29, names_to = "infection", values_to = "count") %>% 
-    mutate(proportion = count/sum) %>% 
-    mutate(infection = str_remove(infection, "I"),
-           infection = replace(infection, infection == "susceptible_reinf", 25)) %>% 
-    filter(infection %in% c(1:25)) %>% 
-    mutate(infection = as.numeric(infection)) %>% 
-    rename(month_num = month, level = infection) %>% 
-    select(-c(sum, count)) %>% 
-    # combining with birth data
-    left_join(stored_data[[3]][, c("births", "time")], by = "time") %>% 
-    mutate(susceptible = proportion * births) %>% 
-    select(-c(proportion, births, month_num, rate)) %>% 
-    arrange(level)
+  # calculate proportion of women in each infection history status per month
+  women[, 3:28] <- women[, 3:28]/1000000
+  babies <- women[, -3] # remove susceptible_naive column since negligible in later time steps
+  
+  # calculate number of babies born with an immunity profile based on births and proportion
+  babies[, 3:27] <- babies[, 3:27] * babies[, 28]
   
   print(Sys.time())
   
   ## model babies
   # apply lambda to rate vector
-  stored_data[[6]][99:111] <- stored_data[[6]][99:111] * lambda
+  # stored_data[[4]][99:111] <- stored_data[[4]][99:111] * lambda
+  # matrix key:
+  # 1 = time_calendar
+  # 2 = rate
+  # 3 - 27 = immunity levels
+  # 28 = births
+  # 29 = time_birth
+  # 30 = aging
+  # 31 = waning
+  # 32 = infected
+  # 33 = disease
   
-  # nesting data set to model
-  births <- immunity_split %>% 
-    nest(.by = time)
-  
-  for(n in 1:216){
-    data <- births[[2]][[n]] %>% 
-      nest(.by = level)
-    
-    for(lev in 1:25){
-      subdata <- stored_data[[5]] %>% mutate(time_calendar = n:(n+12*4-1),
-                                           time_birth = 1:48,
-                                           level = lev,
-                                           rate = stored_data[[6]][n:(n+12*4-1)],
-                                           prob_inf = theta * level,
-                                           waning = omega * time_birth + 1,
-                                           aging = alpha * time_birth + 1)
-      
-      # initial state
-      subdata[1, "susceptible"] <- data[[2]][[lev]]$susceptible
-      
-      for(row in 1:48){
-        subdata[row, "infected"] <- subdata[row, "susceptible"] * subdata[row, "rate"] * (1 - ((1 - subdata[row, "prob_inf"]) * subdata[row, "waning"]))
-        subdata[row, "disease"] <- subdata[row, "susceptible"] * subdata[row, "rate"] * (1 - ((1 - subdata[row, "prob_inf"]) * subdata[row, "waning"])) * subdata[row, "aging"]
-        subdata[row + 1, "susceptible"] <- subdata[row, "susceptible"] - subdata[row, "infected"]
-      }
-      
-      data[[2]][[lev]] <- subdata[1:48, ]
-    }
-    
-    births[[2]][[n]] <- data %>% unnest()
-    
-  }
-  
-  ## convert to desired output form
-  output <- births %>% 
-    unnest(cols = c(data)) %>% 
-    select(-c(time, level)) %>% 
-    rename(level = level1) %>% 
-    mutate(age = case_when(time_birth  < 12 ~ "<1",
-                           time_birth >= 12 & time_birth  <= 48 ~ "1-4")) %>%
-    group_by(time_calendar, age) %>% 
-    summarise(disease = sum(disease)) %>% 
-    ungroup() %>% 
-    mutate(population = case_when(age == "<1" ~ stored_data[[4]][1],
-                                  age == "1-4" ~ stored_data[[4]][2]),
-           rate = (disease/population)*100000) %>% 
-    filter(time_calendar >= 58 & time_calendar <= 149) %>% 
-    arrange(age)
+  data <- map(1:nrow(babies),
+          function(x){
+            
+            subdata <- empty
+            subdata[1, 1:28] <- babies[x, ]
+            subdata[, 1] <- x:(x+12*4-1)
+            subdata[, 2] <- stored_data[[3]][x:(x+12*4-1)]
+            subdata <- cbind(subdata,
+                          waning = omega * subdata[, 29] + 1,
+                          aging = alpha * subdata[, 29] + 1,
+                          infected = 0,
+                          disease = 0)
+            
+            for(month in 2:48){
+              subdata[month, 3:27] <- subdata[month - 1, 3:27] # susceptible babies to next time step
+              subdata[month - 1, 3:27] <- subdata[month - 1, 3:27] * subdata[month - 1, 2] * (1 - ((1 - (theta * stored_data[[5]])) * subdata[month - 1, 30])) # calculate number of babies infected in each immunity level
+              subdata[month - 1, 32] <- sum(subdata[month - 1, 3:27]) # total number of infections at that time step
+              subdata[month, 3:27] <- subdata[month, 3:27] - subdata[month - 1, 3:27] # deduct infected from susceptible
+              subdata[month - 1, 3:27] <- subdata[month - 1, 3:27] * subdata[month - 1, 31] # calculate number of babies that develop disease in each immunity level
+              subdata[month - 1, 33] <- sum(subdata[month - 1, 3:27]) # total number of babies that develop disease at that time step
+            }
+            
+            return(subdata)
+            
+          })
   
   print(Sys.time())
   
-  return(output)
+  data <- do.call(rbind, data)[, c(1, 29, 33)] # unlist map output
+  data <- cbind(data, age = 0)
+  data[data[, 2] > 11, 4] <- 1 # 0 = <1, 1 = 1-4 
+  data <- tapply( data[, 3], list(data[, 1],  data[, 4]), sum) # calculate counts of disease per age group per month
+  data <- cbind(data,
+                rate_0 = 0,
+                rate_1 = 0)
+  data[, 3] <- data[, 1]/stored_data[[4]][2]*100000 # calculate rates for <1
+  data[, 4] <- data[, 2]/stored_data[[4]][1]*100000 # calculate rates for 1-4
+  data <- cbind(data[, 3:4],
+                time = 1:263)
+  data <- rbind(data[58:149, c(1, 3)], data[58:149, 2:3]) # selecting times to match Scottish rate data
+  
+  print(Sys.time())
+  
+  return(data)
   
 }
