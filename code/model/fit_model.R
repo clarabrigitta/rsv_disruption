3747c5924101d90d0953ebf963354d9ad40d8f29
clarabrigitta <clara.brigitta@gmail.com>
2024-07-24 12:19:08 +0100

    plot model fits with uncertainty and parameter assumptions

--- code/model/fit_model.R
+++ code/model/fit_model.R
@@ -10,20 +10,66 @@ scotland_rate <- read.csv("./data/respiratory_age_20240515.csv") %>%
   summarise(rate = sum(rate)) %>% 
   ungroup()
 
-View(cbind(scotland_rate, model = model_function(lambda = 0, theta = 1/25, omega = -1/48, alpha = -1/48, stored_data = save_data, uni = 0.2)[, 1]))
+View(cbind(scotland_rate, model = model_function(lambda = 0, theta = 1/25, omega = -1/48, alpha = -1/48, stored_data = save_data, uni = 0.1)[, 1])) # original test parameter values
 
-# plot scotland vs model rate
-both <- cbind(scotland_rate, model = model_function(lambda = 0.037, theta = 0.041, omega = 0.037, alpha = -1/48, stored_data = save_data, uni = 0.081)[, 1])
+
+# -------------------------------------------------------------------------
+# plots
+# plot scotland vs model rate with 95% credible interval parameter values
+both <- cbind(scotland_rate, 
+              model = model_function(lambda = 0.037, theta = 0.041, omega = 0.037, alpha = 0.037, stored_data = save_data, uni = 0.081)[, 1], # omega and alpha should be negative
+              lower = model_function(lambda = 0.006, theta = 0.036, omega = 0.005, alpha = 0.007, stored_data = save_data, uni = 0.075)[, 1],
+              upper = model_function(lambda = 0.351, theta = 0.046, omega = 0.375, alpha = 0.357, stored_data = save_data, uni = 0.086)[, 1])
 
 ggplot(both) +
   geom_line(aes(x = yearmon, y = rate, colour = age), linetype = 1) +
   geom_line(aes(x = yearmon, y = model, colour = age), linetype = 2) +
-  theme_classic()
+  geom_ribbon(aes(x = yearmon, ymax = upper, ymin = lower, colour = age, fill = age), alpha = 0.3, linetype = 0) +
+  theme_classic() +
+  facet_wrap(~age)
 
-# plot mcmc output
+# plot mcmc traces
 plot(out_uni)
 plot(out1)
 plot(out2)
+plot(out3)
+plot(out4)
+
+# plot parameter assumptions
+theta <- ggplot(data.frame(x = c(0, 25)), aes(x = x)) + 
+  stat_function(fun = function(x){1/25*x}) +
+  scale_x_continuous(breaks = seq(0, 25, 5)) +
+  theme_bw() +
+  labs(title = "Theta (guess)", x = "Immunity level", y = "Probability of infection")
+theta_fit <- ggplot(data.frame(x = c(0, 25)), aes(x = x)) + 
+  stat_function(fun = function(x){0.041*x}) +
+  scale_x_continuous(breaks = seq(0, 25, 5)) +
+  theme_bw() +
+  labs(title = "Theta (fit)", x = "Immunity level", y = "Probability of infection")
+
+omega <- ggplot(data.frame(x = c(0, 48)), aes(x = x)) + 
+  stat_function(fun = function(x){-1/48*x+1}) +
+  theme_bw() +
+  scale_x_continuous(breaks = seq(0, 48, 4)) +
+  labs(title = "Omega (guess)", x = "Months since birth", y = "% change on probability of infection")
+omega_fit <- ggplot(data.frame(x = c(0, 48)), aes(x = x)) + 
+  stat_function(fun = function(x){0.037*x+1}) +
+  theme_bw() +
+  scale_x_continuous(breaks = seq(0, 48, 4)) +
+  labs(title = "Omega (fit)", x = "Months since birth", y = "% change on probability of infection")
+
+alpha <- ggplot(data.frame(x = c(0, 48)), aes(x = x)) + 
+  stat_function(fun = function(x){-1/48*x+1}) +
+  theme_bw() +
+  scale_x_continuous(breaks = seq(0, 48, 4)) +
+  labs(title = "Alpha (guess)", x = "Months since birth", y = "% of developing disease")
+alpha_fit <- ggplot(data.frame(x = c(0, 48)), aes(x = x)) + 
+  stat_function(fun = function(x){0.037*x+1}) +
+  theme_bw() +
+  scale_x_continuous(breaks = seq(0, 48, 4)) +
+  labs(title = "Alpha (fit)", x = "Months since birth", y = "% of developing disease")
+
+grid.arrange(theta, omega, alpha, theta_fit, omega_fit, alpha_fit, ncol = 3, nrow = 2)
 
 # -------------------------------------------------------------------------
 library(BayesianTools)
@@ -44,7 +90,7 @@ likelihood_uni <- function(param){
   return(sum(likelihood))  
 }
 
-setUp_uni <- createBayesianSetup(likelihood_uni, lower = c(0, 0), upper = c(1, 1000))
+setUp_uni <- createBayesianSetup(likelihood_uni, lower = c(0, 0), upper = c(1, 200))
 
 settings = list(iterations = 1000, nrChains = 4, message = TRUE)
 out_uni <- runMCMC(bayesianSetup = setUp_uni, sampler = "Metropolis", settings = settings)
@@ -77,7 +123,7 @@ likelihood1 <- function(param){
 # )
 
 # setting up Bayesian model
-setUp1 <- createBayesianSetup(likelihood1, lower = c(0, 0), upper = c(1, 1000))
+setUp1 <- createBayesianSetup(likelihood1, lower = c(0, 0), upper = c(1, 200))
 
 # run for 10,000 steps
 settings = list(iterations = 1000, nrChains = 4, message = TRUE)
