0e3ba436bde063d4b3f94bced7d9de4dce2a3aa4
clarabrigitta <clara.brigitta@gmail.com>
2024-10-30 11:08:02 +0000

    calculate counts from Scottish rate data; add likelihoods one parameter at a time; model trajectories

--- code/model/fit_model.R
+++ code/model/fit_model.R
@@ -1,3 +1,14 @@
+library(BayesianTools)
+library(posterior)
+library(bayesplot)
+library(ggplot2)
+library(gridExtra)
+library(parallel)
+
+source("~/Desktop/rsv_disruption/code/model/model_function.R")
+
+# -------------------------------------------------------------------------
+
 # weekly rate of laboratory confirmed cases by age and pathogen aggregated to monthly
 scotland_rate <- read.csv("./data/respiratory_age_20240515.csv") %>%
   mutate(date = as.Date(as.character(WeekBeginning), "%Y%m%d")) %>% 
@@ -8,216 +19,194 @@ scotland_rate <- read.csv("./data/respiratory_age_20240515.csv") %>%
   mutate(yearmon = as.yearmon(date)) %>% 
   group_by(age, yearmon) %>% 
   summarise(rate = sum(rate)) %>% 
-  ungroup()
-
-View(cbind(scotland_rate, model = model_function(lambda = 0, theta = 1/25, omega = -1/48, alpha = -1/48, stored_data = save_data, uni = 0.1)[, 1])) # original test parameter values
-
-
-# -------------------------------------------------------------------------
-# plots
-# plot scotland vs model rate with 95% credible interval parameter values
-both <- cbind(scotland_rate, 
-              model = model_function(lambda = 0.037, theta = 0.041, omega = 0.037, alpha = 0.037, stored_data = save_data, uni = 0.081)[, 1], # omega and alpha should be negative
-              lower = model_function(lambda = 0.006, theta = 0.036, omega = 0.005, alpha = 0.007, stored_data = save_data, uni = 0.075)[, 1],
-              upper = model_function(lambda = 0.351, theta = 0.046, omega = 0.375, alpha = 0.357, stored_data = save_data, uni = 0.086)[, 1])
-
-ggplot(both) +
-  geom_line(aes(x = yearmon, y = rate, colour = age), linetype = 1) +
-  geom_line(aes(x = yearmon, y = model, colour = age), linetype = 2) +
-  geom_ribbon(aes(x = yearmon, ymax = upper, ymin = lower, colour = age, fill = age), alpha = 0.3, linetype = 0) +
-  theme_classic() +
-  facet_wrap(~age)
-
-# plot mcmc traces
-plot(out_uni)
-plot(out1)
-plot(out2)
-plot(out3)
-plot(out4)
-
-# plot parameter assumptions
-theta <- ggplot(data.frame(x = c(0, 25)), aes(x = x)) + 
-  stat_function(fun = function(x){1/25*x}) +
-  scale_x_continuous(breaks = seq(0, 25, 5)) +
-  theme_bw() +
-  labs(title = "Theta (guess)", x = "Immunity level", y = "Probability of infection")
-theta_fit <- ggplot(data.frame(x = c(0, 25)), aes(x = x)) + 
-  stat_function(fun = function(x){0.041*x}) +
-  scale_x_continuous(breaks = seq(0, 25, 5)) +
-  theme_bw() +
-  labs(title = "Theta (fit)", x = "Immunity level", y = "Probability of infection")
-
-omega <- ggplot(data.frame(x = c(0, 48)), aes(x = x)) + 
-  stat_function(fun = function(x){-1/48*x+1}) +
-  theme_bw() +
-  scale_x_continuous(breaks = seq(0, 48, 4)) +
-  labs(title = "Omega (guess)", x = "Months since birth", y = "% change on probability of infection")
-omega_fit <- ggplot(data.frame(x = c(0, 48)), aes(x = x)) + 
-  stat_function(fun = function(x){0.037*x+1}) +
-  theme_bw() +
-  scale_x_continuous(breaks = seq(0, 48, 4)) +
-  labs(title = "Omega (fit)", x = "Months since birth", y = "% change on probability of infection")
-
-alpha <- ggplot(data.frame(x = c(0, 48)), aes(x = x)) + 
-  stat_function(fun = function(x){-1/48*x+1}) +
-  theme_bw() +
-  scale_x_continuous(breaks = seq(0, 48, 4)) +
-  labs(title = "Alpha (guess)", x = "Months since birth", y = "% of developing disease")
-alpha_fit <- ggplot(data.frame(x = c(0, 48)), aes(x = x)) + 
-  stat_function(fun = function(x){0.037*x+1}) +
-  theme_bw() +
-  scale_x_continuous(breaks = seq(0, 48, 4)) +
-  labs(title = "Alpha (fit)", x = "Months since birth", y = "% of developing disease")
-
-grid.arrange(theta, omega, alpha, theta_fit, omega_fit, alpha_fit, ncol = 3, nrow = 2)
+  ungroup() %>% 
+  # to calculate counts
+  mutate(population = ifelse(age == "<1 years", 47186, 200551),
+         count = rate / 100000 * population)
 
 # -------------------------------------------------------------------------
-library(BayesianTools)
-library(posterior)
-library(bayesplot)
 
-source("~/Desktop/rsv_disruption/code/model/model_function.R")
-
-# adding likelihood for universal scaling factor for rate of exposure (to make up for difference between model and Scotland data)
-likelihood_uni <- function(param){
-  scale <- param[1]
-  stdev <- param[2]
-  likelihood <- dnorm(scotland_rate$rate,
-                      mean = model_function(lambda = 0, theta = 1/25, omega = -1/48, alpha = -1/48, stored_data = save_data, uni = scale)[, 1],
-                      sd = stdev,
-                      log = T)
+# detection rate likelihood (to make up for difference between model and Scotland data)
+likelihood_detection <- function(param){
+  detection <- param[1]
+  likelihood <- dbinom(round(scotland_rate$count, digits = 0),
+                       size = as.numeric(round(model_function(lambda = 0, theta = 1/25, omega = -1/48, alpha = -1/48, stored_data = save_data)[, 1], digits = 0)),
+                       prob = detection,
+                       log = T)
   
   return(sum(likelihood))  
 }
 
-setUp_uni <- createBayesianSetup(likelihood_uni, lower = c(0, 0), upper = c(1, 200))
+setUp_detection <- createBayesianSetup(likelihood_detection, lower = c(0), upper = c(1))
 
-settings = list(iterations = 1000, nrChains = 4, message = TRUE)
-out_uni <- runMCMC(bayesianSetup = setUp_uni, sampler = "Metropolis", settings = settings)
+settings = list(iterations = 10000, nrChains = 1, message = TRUE)
 
-posterior <- getSample(out_uni)
+Sys.time()
+results <- mclapply(1:4,
+                     function(x) {
+                       runMCMC(bayesianSetup = setUp_detection, sampler = "Metropolis", settings = settings)
+                       },
+                     mc.cores = 12)
+Sys.time()
 
-summary(out_uni)
-plot(out_uni) 
+out_detection <- createMcmcSamplerList(results)
 
-# scaling factor for rate during disruption
+posterior <- getSample(out_detection)
 
-# likelihood function
-likelihood1 <- function(param){
-  disrupt <- param[1]
-  stdev <- param[2]
-  likelihood <- dnorm(scotland_rate$rate,
-                      mean = model_function(lambda = disrupt, theta = 1/25, omega = -1/48, alpha = -1/48, stored_data = save_data, uni = 0.08)[, 1],
-                      sd = stdev,
-                      log = T)
-  
-  return(sum(likelihood))  
-}
+summary(out_detection)
+plot(out_detection) 
 
-# keeping track of time
-# time <- Sys.time()
-# likelihood1(c(0.0, 1.0))
-# Sys.time() - time
-# bench::mark(
-#   likelihood1(c(0.0, 1.0))
-# )
-
-# setting up Bayesian model
-setUp1 <- createBayesianSetup(likelihood1, lower = c(0, 0), upper = c(1, 200))
-
-# run for 10,000 steps
-settings = list(iterations = 1000, nrChains = 4, message = TRUE)
-out1 <- runMCMC(bayesianSetup = setUp1, sampler = "Metropolis", settings = settings)
-
-posterior <- getSample(out1)
-
-summary(out1)
-plot(out1) # plot internally calls tracePlot(out)
-
+# troubleshooting plots
+# library(coda)
+# mcmc_trimmed <- window(out_detection[[1]][["chain"]][[1]], start = 50 +1)
+# traceplot(mcmc_trimmed[, c("par 1")])
 
 # -------------------------------------------------------------------------
 
-# probability of infection given immunity level
-
-# likelihood function
-likelihood2 <- function(param){
-  inf_imm <- param[1]
-  stdev <- param[2]
-  likelihood = dnorm(scotland_rate$rate,
-                     mean = model_function(lambda = 0, theta = inf_imm, omega = -1/48, alpha = -1/48, stored_data = save_data, uni = 0.08)[, 1],
-                     sd = stdev, 
-                     log = T)
+# adding parameters one at a time
+likelihood_two <- function(param){
+  detection <- param[1]
+  disruption <- param[2]
+  likelihood <- dbinom(round(scotland_rate$count, digits = 0),
+                       size = as.numeric(round(model_function(lambda = exp(disruption), theta = 1/25, omega = -1/48, alpha = -1/48, stored_data = save_data)[, 1], digits = 0)),
+                       prob = detection,
+                       log = T)
   
   return(sum(likelihood))  
 }
 
-# setting up Bayesian model
-setUp2 <- createBayesianSetup(likelihood2, lower = c(-1, 0), upper = c(1, 200))
+setUp_2 <- createBayesianSetup(likelihood_two, lower = c(0, -10), upper = c(1, 0))
 
-# run for 100,000 steps
-settings = list(iterations = 1000, nrChains = 4, message = FALSE)
-out2 <- runMCMC(bayesianSetup = setUp2, sampler = "Metropolis", settings = settings)
+settings = list(iterations = 10000, nrChains = 1, message = TRUE)
 
-posterior <- getSample(out2)
+Sys.time()
+results <- mclapply(1:4,
+                    function(x) {
+                      runMCMC(bayesianSetup = setUp_2, sampler = "DEzs", settings = settings)
+                    },
+                    mc.cores = 4)
+Sys.time()
 
-summary(out2)
-plot(out2) # plot internally calls tracePlot(out)
+out_twoDEzs <- createMcmcSamplerList(results)
 
+posterior <- getSample(out_two)
+
+summary(out_two)
+plot(out_two) 
+
+# three parameters
+likelihood_three <- function(param){
+  detection <- param[1]
+  disruption <- param[2]
+  inf_imm <- param[3]
+  likelihood <- dbinom(round(scotland_rate$count, digits = 0),
+                       size = as.numeric(round(model_function(lambda = exp(disruption), theta = inf_imm, omega = -1/48, alpha = -1/48, stored_data = save_data)[, 1], digits = 0)),
+                       prob = detection,
+                       log = T)
+  
+  return(sum(likelihood))  
+}
+
+setUp_3 <- createBayesianSetup(likelihood_three, lower = c(0, -10, 0), upper = c(1, 0, 1))
+
+settings = list(iterations = 10000, nrChains = 1, message = TRUE)
+
+Sys.time()
+results <- mclapply(1:4,
+                    function(x) {
+                      runMCMC(bayesianSetup = setUp_3, sampler = "DEzs", settings = settings)
+                    },
+                    mc.cores = 4)
+Sys.time()
+
+out_threeDEzs <- createMcmcSamplerList(results)
+
+posterior <- getSample(out_three)
+
+summary(out_three)
+plot(out_three) 
+
+# four parameters
+likelihood_four <- function(param){
+  detection <- param[1]
+  disruption <- param[2]
+  inf_imm <- param[3]
+  waning <- param[4]
+  
+  likelihood <- dbinom(round(scotland_rate$count, digits = 0),
+                       size = as.numeric(round(model_function(lambda = exp(disruption), theta = inf_imm, omega = waning, alpha = -1/48, stored_data = save_data)[, 1], digits = 0)),
+                       prob = detection,
+                       log = T)
+  
+  return(sum(likelihood))  
+}
+
+setUp_4 <- createBayesianSetup(likelihood_four, lower = c(0, -10, 0, -1), upper = c(1, 0, 1, 0))
+
+settings = list(iterations = 10000, nrChains = 1, message = TRUE)
+
+Sys.time()
+results <- mclapply(1:4,
+                    function(x) {
+                      runMCMC(bayesianSetup = setUp_4, sampler = "DEzs", settings = settings)
+                    },
+                    mc.cores = 4)
+Sys.time()
+
+out_fourDEzs <- createMcmcSamplerList(results)
+
+posterior <- getSample(out_four)
+
+summary(out_four)
+plot(out_four)
 
 # -------------------------------------------------------------------------
 
-# waning immunity given time since birth/age
-
-# likelihood function
-likelihood3 <- function(param){
-  waning_imm <- param[1]
-  stdev <- param[2]
-  likelihood = dnorm(scotland_rate$rate,
-                     mean = model_function(lambda = 0, theta = 1/25, omega = waning_imm, alpha = -1/48, stored_data = save_data, uni = 0.08)[, 1],
-                     sd = stdev, 
-                     log = T)
+# single likelihood function
+likelihood_all <- function(param){
+  detection <- param[1]
+  disruption <- param[2]
+  inf_imm <- param[3]
+  waning <- param[4]
+  aging <- param[5]
+  
+  likelihood <- dbinom(round(scotland_rate$count, digits = 0),
+                       size = as.numeric(round(model_function(lambda = exp(disruption), theta = inf_imm, omega = waning, alpha = aging, stored_data = save_data)[, 1], digits = 0)),
+                       prob = detection,
+                       log = T)
   
   return(sum(likelihood))  
 }
 
-# setting up Bayesian model
-setUp3 <- createBayesianSetup(likelihood3, lower = c(-1, 0), upper = c(1, 200))
+setUp_all <- createBayesianSetup(likelihood_all, lower = c(0, -10, 0, -1, -1), upper = c(1, 0, 1, 0, 0))
 
-# run for 100,000 steps
-settings = list(iterations = 1000, nrChains = 4, message = TRUE)
-out3 <- runMCMC(bayesianSetup = setUp1, sampler = "Metropolis", settings = settings)
+settings = list(iterations = 10000, nrChains = 1, message = TRUE)
 
-posterior <- getSample(out3)
+Sys.time()
+results <- mclapply(1:4,
+                    function(x) {
+                      runMCMC(bayesianSetup = setUp_all, sampler = "DEzs", settings = settings)
+                    },
+                    mc.cores = 4)
+Sys.time()
 
-summary(out3)
-plot(out3) # plot internally calls tracePlot(out)
+out_allDEzs <- createMcmcSamplerList(results)
 
+posterior <- getSample(out_all)
+
+summary(out_all)
+plot(out_all, which = c(1:4))
 
 # -------------------------------------------------------------------------
+# saving trajectories
 
+posterior <- getSample(out_allDEzs)
+Sys.time()
+traj <- mclapply(1:nrow(posterior),
+                 function(n){
+                   model_function(lambda = exp(posterior[n, 2]), theta = posterior[n, 3], omega = posterior[n, 4], alpha = posterior[n, 5], stored_data = save_data)[, 1] * posterior[n, 1]
+                 },
+                 mc.cores = 4)
+Sys.time()
 
-# aging given time since birth/age
-
-# likelihood function
-likelihood4 <- function(param){
-  age <- param[1]
-  stdev <- param[2]
-  likelihood = dnorm(scotland_rate$rate,
-                     mean = model_function(lambda = 0, theta = 1/25, omega = -1/48, alpha = age, stored_data = save_data, uni = 0.08)[, 1],
-                     sd = stdev, 
-                     log = T)
-  
-  return(sum(likelihood))  
-}
-
-# setting up Bayesian model
-setUp4 <- createBayesianSetup(likelihood4, lower = c(-1, 0), upper = c(1, 200))
-
-# run for 100,000 steps
-settings = list(iterations = 1000, nrChains = 4, message = FALSE)
-out4 <- runMCMC(bayesianSetup = setUp1, sampler = "Metropolis", settings = settings)
-
-posterior <- getSample(out4)
-
-summary(out4)
-plot(out4) # plot internally calls tracePlot(out)
+traj_allDEzs <- traj
