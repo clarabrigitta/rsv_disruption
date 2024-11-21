4418b58b1f04e27d4668334d528116f7278ce6b7
clarabrigitta <clara.brigitta@gmail.com>
2024-11-21 16:49:45 +0000

    add plots for function shapes

--- code/plot/plot_trajectories.R
+++ code/plot/plot_trajectories.R
@@ -50,3 +50,26 @@ paramsampler <- ...
   
   ggsave(filename = paste0("/Users/lsh2301561/Desktop/rsv_disruption/output/figures/trajectories/hdi_", paramsampler, ".png"), plot = fighdi, width = 12, height = 8, dpi = 300)
 # } # only for use if loop needed
+
+
+# -------------------------------------------------------------------------
+
+# plot function shapes
+maternal <- ggplot(data.frame(x = c(0, 25)), aes(x = x)) + 
+    stat_function(fun = function(x){1/(1+exp(-2*(x-25)))}, aes(colour = "k=2, x0=25")) +
+    theme_bw() +
+    labs(x = "Months since maternal infection", y = "Immunity level")
+
+waning <- ggplot(data.frame(x = c(0, 48)), aes(x = x)) + 
+  stat_function(fun = function(x){1/(1+exp(1*(x-45)))}, aes(colour = "k=1, x0=45")) +
+  scale_x_continuous(breaks = seq(0, 48, 4)) +
+  theme_bw() +
+  labs(x = "Months since birth", y = "Waning immunity")
+  
+aging <- ggplot(data.frame(x = c(0, 48)), aes(x = x)) + 
+  stat_function(fun = function(x){1/(1+exp(2*(x-30)))}, aes(colour = "k=2, x0=40")) +
+  scale_x_continuous(breaks = seq(0, 48, 4)) +
+  theme_bw() +
+  labs(x = "Months since birth", y = "Probability of disease")
+
+grid.arrange(maternal, aging, waning, ncol = 3, nrow = 1)
