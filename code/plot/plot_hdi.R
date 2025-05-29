845009e4b4fd9b4cbb43262b8007b1cbb7211e0b
clarabrigitta <clara.brigitta@gmail.com>
2025-05-29 11:59:36 +0100

    fix plot formatting

--- code/plot/plot_hdi.R
+++ code/plot/plot_hdi.R
@@ -6,7 +6,7 @@ library(HDInterval)
 
 plot_hdi <- function(traj, traj_infection){
   
-  traj <- do.call(rbind, traj) %>% 
+  data <- do.call(rbind, traj) %>% 
     as.data.frame() %>% 
     hdi() %>% 
     t() %>% 
@@ -20,20 +20,24 @@ plot_hdi <- function(traj, traj_infection){
                 as.data.frame() %>% 
                 rename(lower_inf = lower, upper_inf = upper))
   
-  fig <- ggplot(traj) +
+  fig <- ggplot(data) +
     geom_line(aes(x = yearmon, y = median_inf, color = "Infection"), linetype = 1) +
     geom_ribbon(aes(x = yearmon, ymax = upper_inf, ymin = lower_inf, fill = "Infection"), alpha = 0.3, linetype = 0) +
     geom_line(aes(x = yearmon, y = median, color = "Disease"), linetype = 1) +
     geom_ribbon(aes(x = yearmon, ymax = upper, ymin = lower, fill = "Disease"), alpha = 0.3, linetype = 0) +
-    geom_point(aes(x = yearmon, y = count, shape = "Scottish Data"), size = 1, colour = "black") +
+    geom_point(aes(x = yearmon, y = count, shape = "Lab-confirmed cases\nfrom Scotland"), size = 2, colour = "black") +
     scale_color_manual(name = "", values = c("Infection" = "#2A7B43", "Disease" = "#C93842")) +
     scale_fill_manual(name = "", values = c("Infection" = "#2A7B43", "Disease" = "#C93842")) +
-    scale_shape_manual(name = "", values = c("Scottish Data" = 16)) +
-    labs(x = "Months", y = "Count") + 
+    scale_shape_manual(name = "", values = c("Lab-confirmed cases\nfrom Scotland" = 16)) +
+    labs(x = "Time (months)", y = "Number of RSV cases") + 
     theme_classic() +
+    theme(axis.text=element_text(size=12),
+          axis.title=element_text(size=14),
+          strip.text = element_text(size = 14, face = "bold"),
+          legend.text = element_text(size = 12)) + 
     facet_wrap(~age)
   
   dir.create(here("output", "figures", "hdi", format(Sys.Date(), "%d%m%Y")))
-  ggsave(filename = here("output", "figures", "hdi", format(Sys.Date(), "%d%m%Y"), paste0(n, ".png")), plot = fig, width = 9, height = 4, dpi = 300)
+  ggsave(filename = here("output", "figures", "hdi", format(Sys.Date(), "%d%m%Y"), paste0(n, ".png")), plot = fig, width = 11, height = 5, dpi = 300)
   
 }
