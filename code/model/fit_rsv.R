c8a22d24f7349f3f76fcd6cd623ff722309b9b51
clarabrigitta <156320564+clarabrigitta@users.noreply.github.com>
2025-12-09 11:00:30 +0000

    explain combinations n

--- code/model/fit_rsv.R
+++ code/model/fit_rsv.R
@@ -47,7 +47,7 @@ scotland_rate <- read.csv(here("data", "respiratory_age_20241218.csv")) %>%
 # create combinations to run
 combinations <- create_combinations()
 
-for(n in c(17, 19:21)){
+for(n in c(17)){ # 17 is baseline scenario, other n refers to different exploratory sensitivity analyses
   
   # set duration of maternal immunity
   duration = combinations[[n]]$duration
