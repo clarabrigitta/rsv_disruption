5ceb75048b2da4f2b061a97855107738378255f3
clarabrigitta <156320564+clarabrigitta@users.noreply.github.com>
2025-12-09 13:53:26 +0000

    name supplement plots and remove test code

--- code/plot/plot_supplements.R
+++ code/plot/plot_supplements.R
@@ -1,5 +1,57 @@
 library(patchwork)
 
+# plot rates for entire study period and one year
+rate <- ((dates %>% 
+            distinct() %>% 
+            filter(yearmon >= "Jul 2010", yearmon <= "Jul 2011") %>% 
+            mutate(rate = rate + 0.0075) %>% 
+            ggplot() +
+            geom_line(aes(x = time_calendar, y = rate)) +
+            scale_x_continuous(breaks = seq(7, 19, 1), labels = c(month.abb[7:12], month.abb[1:7])) +
+            theme_bw() +
+            labs(y = "Rate of RSV Exposure (\u03bb)",
+                 x = "Month of Year") +
+            theme(axis.text=element_text(size=12),
+                  axis.title=element_text(size=14))) / (dates %>% 
+                                                          distinct() %>% 
+                                                          filter(yearmon >= "Oct 2016", yearmon <= "Oct 2024") %>% 
+                                                          mutate(rate = rate + 0.0075) %>% 
+                                                          mutate(rate = ifelse(yearmon >= "Mar 2020" & yearmon <= "Mar 2021", rate * exp(-4), rate)) %>% 
+                                                          ggplot() +
+                                                          geom_line(aes(x = yearmon, y = rate)) +
+                                                          theme_bw() +
+                                                          labs(y = "Rate of RSV Exposure (\u03bb)",
+                                                               x = "Month") +
+                                                          theme(axis.text=element_text(size=12),
+                                                                axis.title=element_text(size=14))) + plot_annotation(tag_levels = "A"))
+
+# plot births for study period
+births <- (birth_data %>% 
+  filter(yearmon >= "Jan 2010") %>% 
+  ggplot() +
+  geom_line(aes(x = yearmon, y = births)) +
+  theme_bw() +
+  scale_x_yearmon(breaks = seq(from = as.yearmon("Jan 2010"), 
+                               to = as.yearmon("Oct 2024"), 
+                               by = 1)) +
+  labs(x = "Time (months)",
+       y = "Number of births") +
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=14),
+        axis.text.x = element_text(angle = 45, hjust = 1))) / (birth_data %>% 
+  filter(yearmon >= "Jan 2010") %>% 
+  ggplot() +
+  geom_line(aes(x = yearmon, y = births)) +
+  theme_bw() +
+  scale_x_yearmon(breaks = seq(from = as.yearmon("Jan 2010"), 
+                               to = as.yearmon("Feb 2025"), 
+                               by = 1)) +
+  labs(x = "Time (months)",
+       y = "Number of births") +
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=14),
+        axis.text.x = element_text(angle = 45, hjust = 1))) +  plot_annotation(tag_levels = "A") + theme(plot.tag = element_text(size = 14))
+
 # extract posteriors for sensitivity analysis on duration of immunity
 n = 19
 out <- readRDS(here("output", "data", "parameters", "15032025*", paste0("out", n, ".rds")))
@@ -42,63 +94,15 @@ out <- readRDS(here("output", "data", "parameters", "15032025*", paste0("out", n
 
 posterior <- getSample(out, thin = 100)
 posterior <- posterior[1:2000, ]
-fixed <- matrix(combinations[[n]]$fixed[!combinations[[n]]$ind],
-                nrow = nrow(posterior), 
-                ncol = sum(!combinations[[n]]$ind),
-                byrow = TRUE,
-                dimnames = list(NULL, combinations[[n]]$name[!combinations[[n]]$ind]))
+fixed <- matrix(combinations[[n]]$fixed[!combinations[[n]]$indq
+],
+nrow = nrow(posterior), 
+ncol = sum(!combinations[[n]]$ind),
+byrow = TRUE,
+dimnames = list(NULL, combinations[[n]]$name[!combinations[[n]]$ind]))
 posterior <- cbind(posterior, fixed)
 
-rate <- ((dates %>% 
-            distinct() %>% 
-            filter(yearmon >= "Jul 2010", yearmon <= "Jul 2011") %>% 
-            mutate(rate = rate + 0.0075) %>% 
-            ggplot() +
-            geom_line(aes(x = time_calendar, y = rate)) +
-            scale_x_continuous(breaks = seq(7, 19, 1), labels = c(month.abb[7:12], month.abb[1:7])) +
-            theme_bw() +
-            labs(y = "Rate of RSV Exposure (\u03bb)",
-                 x = "Month of Year") +
-            theme(axis.text=element_text(size=12),
-                  axis.title=element_text(size=14))) / (dates %>% 
-                                                          distinct() %>% 
-                                                          filter(yearmon >= "Oct 2016", yearmon <= "Oct 2024") %>% 
-                                                          mutate(rate = rate + 0.0075) %>% 
-                                                          mutate(rate = ifelse(yearmon >= "Mar 2020" & yearmon <= "Mar 2021", rate * exp(-4), rate)) %>% 
-                                                          ggplot() +
-                                                          geom_line(aes(x = yearmon, y = rate)) +
-                                                          theme_bw() +
-                                                          labs(y = "Rate of RSV Exposure (\u03bb)",
-                                                               x = "Month") +
-                                                          theme(axis.text=element_text(size=12),
-                                                                axis.title=element_text(size=14))) + plot_annotation(tag_levels = "A"))
-
-births <- (birth_data %>% 
-  filter(yearmon >= "Jan 2010") %>% 
-  ggplot() +
-  geom_line(aes(x = yearmon, y = births)) +
-  theme_bw() +
-  scale_x_yearmon(breaks = seq(from = as.yearmon("Jan 2010"), 
-                               to = as.yearmon("Oct 2024"), 
-                               by = 1)) +
-  labs(x = "Time (months)",
-       y = "Number of births") +
-  theme(axis.text=element_text(size=12),
-        axis.title=element_text(size=14),
-        axis.text.x = element_text(angle = 45, hjust = 1))) / (birth_data %>% 
-  filter(yearmon >= "Jan 2010") %>% 
-  ggplot() +
-  geom_line(aes(x = yearmon, y = births)) +
-  theme_bw() +
-  scale_x_yearmon(breaks = seq(from = as.yearmon("Jan 2010"), 
-                               to = as.yearmon("Feb 2025"), 
-                               by = 1)) +
-  labs(x = "Time (months)",
-       y = "Number of births") +
-  theme(axis.text=element_text(size=12),
-        axis.title=element_text(size=14),
-        axis.text.x = element_text(angle = 45, hjust = 1))) +  plot_annotation(tag_levels = "A") + theme(plot.tag = element_text(size = 14))
-
+# plot sensitivity analysis for duration of immunity
 sensitivity_duration <- posterior %>% 
   as.data.frame() %>% 
   pivot_longer(1:6, names_to = "parameters") %>% 
@@ -132,6 +136,7 @@ sensitivity_duration <- posterior %>%
   theme(axis.text=element_text(size=12),
         axis.title=element_text(size=14))
 
+# plot infection history in mothers with burn-in
 burnin <- women %>% 
   as.data.frame() %>% 
   pivot_longer(c("I1":paste0("I", n_interest), "susceptible_naive", "susceptible_reinf"), names_to = "infection", values_to = "count") %>% 
@@ -153,17 +158,12 @@ burnin <- women %>%
        y = "Count",
        fill = "Infection Status")
 
+# plot sensitivity analysis for disruption scenario
 sensitivity_contacts <- posterior17 %>% 
   as.data.frame() %>% 
   pivot_longer(1:6, names_to = "parameters") %>% 
   mutate(contact = "12-month reduction") %>% 
   select(-c(detection, disruption)) %>% 
-  # bind_rows(posterior52 %>% 
-  #             as.data.frame() %>% 
-  #             pivot_longer(1:6, names_to = "parameters") %>% 
-  #             mutate(contact = "fixed reduction and gradual increase") %>% 
-  #             select(-c(detection, disruption)) %>% 
-  #             mutate(value = as.numeric(value))) %>% 
   bind_rows(posterior57 %>% 
               as.data.frame() %>% 
               mutate(disruption = exp(disruption)) %>% 
@@ -188,28 +188,7 @@ sensitivity_contacts <- posterior17 %>%
   theme(axis.text=element_text(size=12),
         axis.title=element_text(size=14))
 
-disruption_scenarios <- data.frame(time = 1:25,
-           month = seq(as.Date("2020-03-01"), as.Date("2022-03-01"), by = "month"),
-           ori = c(rep(exp(-4.3), 13), rep(1, 25 - 13)),
-           alt = c(rep(0.01, 2), seq(0.01, 0.1, length.out = 3), rep(0.1, 1), seq(0.1, 0.01, length.out = 5), rep(0.01, 2), seq(0.01, 1, length.out = 4), rep(NA, 25-17)),
-           alt_old = exp(c(rep(-5, 2), seq(-5, red_exp, length.out = 3), rep(red_exp, 2), seq(red_exp, -5, length.out = 4), rep(-5, 3), seq(-5, 0, length.out = 11))),
-           scale_scot1 = exp(c(rep(-5, 2), seq(-5, red_exp, length.out = 3), rep(red_exp, 2), seq(red_exp, -5, length.out = 4), rep(-5, 3), seq(-5, 0, length.out = 11))),
-           scale_scot2 = exp(c(rep(-5, 2), seq(-5, -2.302585, length.out = 3), rep(-2.302585, 2), seq(-2.302585, -5, length.out = 4), rep(-5, 3), seq(-5, 0, length.out = 11))),
-           scale_scot3 = c(rep(0.001, 2), seq(0.001, red_lin, length.out = 3), rep(red_lin, 2), seq(red_lin, 0.001, length.out = 4), rep(0.001, 3), seq(0.001, 1, length.out = 11)),
-           scale_scot4 = c(rep(0.001, 2), seq(0.001, 0.1, length.out = 3), rep(0.1, 2), seq(0.1, 0.001, length.out = 4), rep(0.001, 3), seq(0.001, 1, length.out = 11))) %>% 
-  pivot_longer(cols = 3:8) %>% 
-  # mutate(name = factor(name, levels = c("ori", "alt"))) %>% 
-  ggplot() +
-  geom_line(aes(x = month, y = value, colour = name)) +
-  scale_x_date(date_breaks = "6 month") +
-  # scale_colour_viridis_d(labels = c("ori" = "12-month reduction", "alt" = "fitted reduction and gradual increase")) +
-  labs(x = "Month",
-       y = "Value",
-       colour = "Lockdown Lifting\nScenario") +
-  theme_bw() +
-  theme(axis.text=element_text(size=12),
-        axis.title=element_text(size=14))
-
+# plot disruption scenarios over time
 disruption <- as.data.frame(matrix(nrow = 17, ncol = 2000))
 for(r in 1:nrow(as.data.frame(posterior57))){
   disruption[, r] <- c(rep(0.01, 2), seq(0.01, exp(as.data.frame(posterior57)[r , "disruption"]), length.out = 3), rep(exp(as.data.frame(posterior57)[r , "disruption"]), 1), seq(exp(as.data.frame(posterior57)[r , "disruption"]), 0.01, length.out = 5), rep(0.01, 2), seq(0.01, 1, length.out = 4))
@@ -245,5 +224,6 @@ ggsave(filename = here("output", "figures", "supplements", format(Sys.Date(), "%
 ggsave(filename = here("output", "figures", "supplements", format(Sys.Date(), "%d%m%Y"), "births.png"), plot = births, width = 8, height = 6, dpi = 300)
 ggsave(filename = here("output", "figures", "supplements", format(Sys.Date(), "%d%m%Y"), "sensitivity_contacts.png"), plot = sensitivity_contacts, width = 10, height = 6, dpi = 300)
 ggsave(filename = here("output", "figures", "supplements", format(Sys.Date(), "%d%m%Y"), "burnin.png"), plot = burnin, width = 10, height = 6, dpi = 300)
+ggsave(filename = here("output", "figures", "supplements", format(Sys.Date(), "%d%m%Y"), "sensitivity_duration.png"), plot = sensitivity_duration, width = 10, height = 6, dpi = 300)
 ggsave(filename = here("output", "figures", "supplements", format(Sys.Date(), "%d%m%Y"), "disruption_scenarios.png"), plot = disruption_processed, width = 10, height = 6, dpi = 300)
 
